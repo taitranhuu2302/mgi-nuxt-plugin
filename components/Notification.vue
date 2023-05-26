b970f1c247b7f14ba7908e0b7841cb12fc052735
taitranhuu2302 <tai.tranhuu2002@gmail.com>
2023-05-27 02:03:20 +0700

    update

--- components/Notification.vue
+++ components/Notification.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 const props = defineProps<{
-  notify: INotification
+  notify: INotification;
 }>();
-const {removeNotification} = useNotifications()
-
+const { removeNotification } = useNotifications();
 </script>
 
 <template>
-  <div class="relative bg-white rounded overflow-hidden w-full max-w-[350px] min-h-[80px] shadow-lg items-center flex gap-1">
-    <div class="w-[5px] rounded h-full" :class="props.notify.type"/>
-    <button @click="removeNotification(props.notify.id)" class="absolute top-[5px] right-[5px]">
-      <Icon name="ic:outline-close" size="20"/>
+  <div id="root" :class="props.notify.type">
+    <button
+      @click="removeNotification(props.notify.id)"
+      class="absolute top-[5px] right-[5px]"
+    >
+      <Icon name="ic:outline-close" size="20" />
     </button>
-    <div class="p-3 flex gap-3 items-center">
+    <div class="px-5 w-full py-3 flex gap-3 items-center">
       <div class="wrapper-icon" :class="props.notify.type">
-        <Icon name="ic:round-check" size="20px"/>
+        <Icon name="ic:round-check" size="20px" />
       </div>
       <div>
         <h4 class="text-lg font-semibold">{{ props.notify.title }}</h4>
@@ -25,6 +26,30 @@ const {removeNotification} = useNotifications()
 </template>
 
 <style scoped>
+#root {
+  @apply relative bg-white rounded overflow-hidden w-full max-w-[350px] shadow-lg items-center flex gap-1;
+}
+
+#root::after {
+  content: "";
+  position: absolute;
+  width: 5px;
+  height: 100%;
+}
+
+#root::after.info {
+  @apply bg-blue-500;
+}
+#root::after.success {
+  @apply bg-green-500;
+}
+#root::after.error {
+  @apply bg-red-500;
+}
+#root::after.warning {
+  @apply bg-amber-500;
+}
+
 .wrapper-icon {
   @apply rounded-full w-[30px] h-[30px] text-white flex items-center justify-center;
 }
